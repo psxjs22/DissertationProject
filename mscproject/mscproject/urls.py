a522df706ea65ba405dd3e026c2592212654b763
Jemma Smith <jemma.t.smith@gmail.com>
2023-07-26 20:37:37 +0100

    Create base HTML template

--- mscproject/mscproject/urls.py
+++ mscproject/mscproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import basicapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', basicapp.views.index)
 ]
