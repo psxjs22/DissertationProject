9870d411c332a6e4935576df9c3bb7e21e37ede1
Jemma Smith <jemma.t.smith@gmail.com>
2023-08-21 09:16:24 +0100

    update demographic admin view

--- mscproject/basicapp/models.py
+++ mscproject/basicapp/models.py
@@ -23,16 +23,82 @@ class Participant(models.Model):
     def __str__(self):
         return f"Participant ID: {self.id}, ConsentForm ID: {self.consent_form.id}"
 
+
+GENDER_LIST = (['1', 'Male'], ['2', 'Female'], ['3', 'Non-Binary'], ['4', 'Other'], ['5', 'Prefer not to say'])
+
+AGE_LIST = (['1', '18-24'], ['2', '25-29'], ['3', '30-34'],
+            ['4', '35-39'], ['5', '40-44'], ['6', '45-49'],
+            ['7', '50-54'], ['8', '55-59'], ['9', '60-64'],
+            ['10', '65-69'], ['11', '70-74'], ['12', '75-79'],
+            ['13', '80-84'], ['14', 'Age 85 and over'])
+
+ETHNICITY_LIST = (('Asian or Asian British',
+                   (('1', 'Indian'),
+                    ('2', 'Pakistani'),
+                    ('3', 'Bangladeshi'),
+                    ('4', 'Chinese'),
+                    ('5', 'Any other Asian background'))),
+                ('Black or Black British',
+                   (('1', 'Caribbean'),
+                    ('2', 'African'),
+                    ('3', 'Any other Black, Black British or Caribbean background'))),
+                ('Mixed or multiple ethnic groups',
+                   (('1', 'White and Black Caribbean'),
+                    ('2', 'White and Black African'),
+                    ('3', 'White and Asian'),
+                    ('4', 'Any other Mixed or multiple ethnic background'))),
+                ('White',
+                   (('1', 'English, Welsh, Scottish, Northern Irish or British'),
+                    ('2', 'Irish'),
+                    ('3', 'Gypsy or Irish Traveller'),
+                    ('4', 'Roma'),
+                    ('5', 'Any other White background'))),
+                ('Other ethnic group',
+                   (('1', 'Arab'),
+                    ('2', 'Any other ethnic group'))))
+
+OCCUPATION_LIST = (['1', 'Managers, Directors and Senior Officials'],
+                   ['2', 'Professional Occupations'],
+                   ['3', 'Associate Professional and Technical Occupations'],
+                   ['4', 'Administrative and Secretarial Occupations'],
+                   ['5', 'Skilled Trades Occupations'],
+                   ['6', 'Caring, Leisure and Other Service Occupations'],
+                   ['7', 'Sales and Customer Service Occupations'],
+                   ['8', 'Process, Plant and Machine Operatives'],
+                   ['9', 'Elementary Occupations'],
+                   ['10', 'Retired'],
+                   ['11', 'Full Time Student'],
+                   ['12', 'Not in Employment'],
+                   ['13', 'Other'])
+
+
+EDUCATION_LIST = (['1', 'Primary School'],
+                  ['2', 'Secondary School'],
+                  ['3', 'Higher or secondary or further education (A-levels, BTEC, professional certificate.)'],
+                  ['4', 'University, of equivalent'],
+                  ['5', 'Post-graduate degree'],
+                  ['6', 'Other'],
+                  ['7', 'Prefer not to say'])
+
+
 class Demographic(models.Model):
+
+    gender_choices = list(GENDER_LIST)
+    age_choices = list(AGE_LIST)
+    ethnicity_choices = list(ETHNICITY_LIST)
+    occupation_choices = list(OCCUPATION_LIST)
+    education_choices = list(EDUCATION_LIST)
+
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=10)
-    age = models.IntegerField()
-    ethnicity = models.CharField(max_length=50)
-    occupation = models.CharField(max_length=100)
-    education = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10, choices=gender_choices)
+    age = models.IntegerField(choices=age_choices)
+    ethnicity = models.CharField(max_length=50, choices=ethnicity_choices)
+    occupation = models.CharField(max_length=100, choices=occupation_choices)
+    education = models.CharField(max_length=100, choices=education_choices)
 
     def __str__(self):
-        return f"Demographics for Participant ID: {self.participant_id}"
+        return f"Demographics for Participant ID: {self.participant_id}, Gender: {self.get_gender_display()}, Age: {self.get_age_display()}, Ethnicity: {self.get_ethnicity_display()}, Occupation: {self.get_occupation_display()}, Education: {self.get_education_display()}"
+
 
 class Question(models.Model):
     summary = models.TextField()
